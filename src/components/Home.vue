e39bfbc34b96c8c371050dca062318d504ff728e
Luciano Nahuel Vitale <lucianonahuelvitale@gmail.com>
2021-07-22 20:02:54 -0300

    Added file input and done method to process subtitles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <v-container fluid>
+    <v-form>
+      <v-file-input
+        label="Seleccioná los subtítulos"
+        prepend-icon="mdi-message-text"
+        outlined
+        append-outer-icon="mdi-send"
+        multiple
+        chips
+        v-model="files"
+        @click:append-outer="processSubtitles" />
+    </v-form>
+
     <div class="pills">
       <Pill v-for="word in groupedWords" :key="word.name"
         :name="word.name" :amount="word.amount" />
@@ -14,14 +26,19 @@ export default {
   components: { Pill },
   data: function () {
     return {
+      files: [],
       groupedWords: [
         { name: 'i', amount: 1234 },
         { name: 'you', amount: 900 },
         { name: 'he', amount: 853 },
       ]
     }
+  },
+  methods: {
+    processSubtitles() {
+      console.log(this.files)
+    }
   }
-
 }
 </script>
 
