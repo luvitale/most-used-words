6c32906fd4dfa014b2e611318753b27c876f074d
Luciano Nahuel Vitale <lucianonahuelvitale@gmail.com>
2021-07-22 05:36:25 -0300

    Done log message and change window title when it did finish load

--- src/background.js
+++ src/background.js
@@ -26,13 +26,19 @@ async function createWindow() {
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
     if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
+
+  win.webContents.on('did-finish-load', () => {
+    const { title, version } = require('../package.json')
+    win.setTitle(`${title} :: ${version}`)
+    console.log('Did Finish Load')
+  })
 }
 
 // Quit when all windows are closed.
